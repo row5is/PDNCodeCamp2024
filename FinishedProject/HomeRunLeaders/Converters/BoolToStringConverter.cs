90211573ac30196804ed28b95c6a556fd538f257
Robert Keiser <rob@row-5.com>
2024-04-18 14:18:27 -0400

    Details and Converters

---
+++ FinishedProject/HomeRunLeaders/Converters/BoolToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRunLeaders.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool boolValue)
+            {
+                return boolValue ? "Active" : "Inactive";
+            }
+            return string.Empty;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { throw new NotImplementedException(); }
+    }
+}
